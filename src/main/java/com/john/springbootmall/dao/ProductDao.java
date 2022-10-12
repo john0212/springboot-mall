5b03443830c78aff4bd36e5a7e780c55e8864d52
john0212 <df850044@gmail.com>
2022-10-12 14:35:59 +0800

    商品功能 - 實作查詢商品

---
+++ src/main/java/com/john/springbootmall/dao/ProductDao.java
@@ -0,0 +1,8 @@
+package com.john.springbootmall.dao;
+
+import com.john.springbootmall.model.Product;
+
+public interface ProductDao {
+
+    Product getProductById(Integer productId);
+}
---
+++ src/main/java/com/john/springbootmall/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.john.springbootmall.service;
+
+import com.john.springbootmall.model.Product;
+
+public interface ProductService {
+
+    Product getProductById(Integer productId);
+}
