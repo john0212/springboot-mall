a53b5c331afe2e2994d30167f9f38fe55cfabe25
john0212 <df850044@gmail.com>
2022-10-17 13:43:15 +0800

    帳號功能 - 註冊新帳號

---
+++ src/main/java/com/john/springbootmall/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.john.springbootmall.dao;
+
+import com.john.springbootmall.dto.UserRegisterRequest;
+import com.john.springbootmall.model.User;
+
+public interface UserDao {
+    Integer createUser(UserRegisterRequest userRegisterRequest);
+
+    User getUserById(Integer userId);
+}
